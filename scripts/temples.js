26c48f73be0d76103d3bb9a2c329707e71764df9
penina faafiu <pmfaafiu@gmail.com>
2024-06-23 10:33:05 -0700

    updates6.23

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,1 +1,11 @@
 alert(document.lastModified);
+
+const mainnav = 
+document.querySelector('.navigation')
+const hambutton =
+document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});

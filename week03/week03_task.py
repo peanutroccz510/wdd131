61e77854bbaaca513f301008b6c514716f66c88d
peanutroccz510 <peanutroccz510@yahoo.com>
2025-07-21 20:34:44 -0700

    final_commit_week02

---
+++ week03/week03_task.py
@@ -0,0 +1,20 @@
+import math
+
+
+let scores = [100, 72, 83, 94, 88, 87];
+let accumulator = 0;
+let count = 0;
+
+scores.forEach(score => {
+  if (score > 87) {
+    accumulator += score;
+    count ++;
+  }
+});
+if ( count > 0 ) {
+  console.log(accumulator/count); 
+} else {
+  console.log("No scores reported.");
+}
+
+ 
